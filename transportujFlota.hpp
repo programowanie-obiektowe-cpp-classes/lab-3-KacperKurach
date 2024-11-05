72299120a3595d80cfd95ed80b88a20ddec5d080
KacperK <kurachkacper01@gmail.com>
2024-11-05 01:39:48 +0100

    zaliczenie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,21 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    unsigned int totalTransported = 0;  
+    unsigned int sailboatCount = 0;     
+
+    Stocznia stocznia;  
+
+    while (totalTransported < towar) {
+        Statek* statek = stocznia();
+
+        totalTransported += statek->transportuj();
+
+        if (dynamic_cast<Zaglowiec*>(statek)) {
+            ++sailboatCount;  
+        }
+
+        delete statek; 
+    }
+    return sailboatCount;
 }
